a48c9a6b613e32a698b9ba0dfbe40bc95a9cabbc
alex hill <hill.alexandermarshall@gmail.com>
2020-06-23 16:34:21 -0600

    First Commit

---
+++ bob.js
@@ -0,0 +1,43 @@
+//
+// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const hey = (message) => {
+
+  message = message.trim();
+
+  if (askedQuestion(message)) {
+    return "Sure.";
+  }
+
+  else if (yelledAt(message)) {
+    return "Whoa, chill out!";
+  }
+ 
+  else if (silentTreatment(message)) {
+    return "Fine. Be that way!";
+  }
+
+  else {
+    return "Whatever."
+  }
+};
+
+function askedQuestion(message) {
+  return message.endsWith('?');
+}
+
+function yelledAt(message) {
+  return (message.match(/[A-Za-z]+/) && message == message.toUpperCase());
+  
+}
+
+function silentTreatment(message) {
+  return (message.length == 0);
+}
+
+// function forcefulQuestion(message) {
+//   return (message.yelledAt(-1) == "?")
+// }
+
